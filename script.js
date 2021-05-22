0d89327895369ed5960ff9ab1e70900bba923b17
Denys <40178797+DenisTvardovskiy@users.noreply.github.com>
2021-05-22 12:25:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+let node = null
+let nodeXHR= null
+let formData = {
+    checkin: null,
+    checkout: null,
+    adultsCount: 0,
+    childrenCount: 0
+}
+let URL = `https://ecospace.org.ua:3030/pod/getPods`
+let bod = document.getElementById("content")
+const xhr = new XMLHttpRequest();
+async function getPodsInfo(){
+
+    node = await fetch(URL, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json',
+        
+
+        },
+    body: JSON.stringify(formData)})
+        .then(response => response.json())
+        .then(data => node = data );
+
+    return node
+}
+
+ function getXHR(){
+    const json = JSON.stringify(formData)
+    xhr.open("POST", URL);
+    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    xhr.send(json);
+    xhr.onload = function (){
+        bod.innerHTML=''
+        let result = 'Xhr Data <br>---------------<br>'
+        nodeXHR = JSON.parse(xhr.response)
+        for( let i =0; i<nodeXHR.pods.length; i++){
+            result+=`<li>${nodeXHR.pods[i].title}</li>`
+        }
+        bod.innerHTML = result
+        return nodeXHR
+    }
+}
+
+async function Hello() {
+    bod.innerHTML=''
+    let result = 'Fetch Data <br>---------------<br>'
+    await getPodsInfo()
+    for( let i =0; i<node.pods.length; i++){
+        result+=`<li>${node.pods[i].title}</li>`
+    }
+    bod.innerHTML = result
+}
+
